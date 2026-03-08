d9ccb1442edbd37817fec32bbd51d5f51de2c78e
ryan3465 <ryan3465@gmail.com>
2026-03-08 19:02:09 +0000

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,35 @@
-// Import Component decorator and signal function for reactive state management
-import { Component, signal } from '@angular/core';
-// Import RouterOutlet to display routed components
+// list of imports
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './Services/data-service';
 
-// Define this class as an Angular component with metadata
+// this is the main component of the app, which is used to display the data collected from the APIs and to navigate between the different pages of the app
 @Component({
-  // CSS selector used to embed this component in HTML (e.g., <app-root></app-root>)
   selector: 'app-root',
-  // Array of components/directives this component can use
   imports: [RouterOutlet],
-  // Path to the component's HTML template file
   templateUrl: './app.html',
-  // Path to the component's CSS stylesheet
   styleUrl: './app.css'
 })
-export class App {
-  // Create a reactive signal to store and track the app title
-  // 'protected readonly' prevents external modification and hides from public API
-  protected readonly title = signal('data-app');
+// this class is used to collect the data from the APIs and to display it in the app, specifically student data from the api it uses oninit to collect the data when the app is initialized and stores it in the students variable,
+export class App implements OnInit{
+  students:any[]=[];
+  weatherData: any;
+  constructor(private dataService:DataService){}
+  ngOnInit(): void {
+    this.dataService.GetStudentData().subscribe((data) => {
+      this.students = data.students;
+// this is used to display the student data in the console for testing purposes
+      console.log(this.students);
+    });
+
+    // this is similar to the previous method but it collects the weather data from the openweathermap api and displays it in the app
+    this.dataService.GetweatherData().subscribe((data) => {
+        this.weatherData = data;
+        console.log(this.weatherData);
+
+      });
+
+
+
 }
+}///this is app.ts
